4139c892793424b8133d1c5248bb8316e4aee977
suhashollakc <suhashollakc@gmail.com>
2021-11-30 12:08:22 -0600

    test

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@
   images:{
     domains: ["links.papareact.com","image.tmdb.org","cdn.arstechnica.net","img1.hulu.com"],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+  // webpack: (config, { isServer }) => {
+  //   // Fixes npm packages that depend on `fs` module
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: 'empty'
+  //     }
+  //   }
 
-    return config
-  }
+  //   return config
+  // }
 };
